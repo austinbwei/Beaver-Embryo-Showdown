9023843a0227b3daa016bd608108b3d9dafe56f7
aweiler97 <aweiler@unca.edu>
2019-02-20 12:42:05 -0500

    Character Movement and Rotation
    Added blueprints for characters, movement, rotation, and camera positioning

---
+++ BeaverEmbryoShowdown/Source/BeaverEmbryoShowdown/BaseCharacter.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BaseCharacter.generated.h"
+
+UCLASS(Blueprintable)
+class BEAVEREMBRYOSHOWDOWN_API ABaseCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	//Health property
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Base Character")
+		float Health = 100;
+
+	//isDead
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Base Character")
+		bool isDead = false;
+
+	//Calculate death
+	virtual void CalculateDead();
+
+	//Calculate health
+	UFUNCTION(BlueprintCallable, Category = "Base Character")
+		virtual void CalculateHealth(float delta);
+
+	//Changing properties in editor
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+		override;
+#endif
+
+public:
+	// Sets default values for this character's properties
+	ABaseCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
